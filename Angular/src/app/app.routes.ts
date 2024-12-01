2cc1157ee93177521dce5504d434cd999f707eaf
OttonyKazumi <ottony@hotmail.com>
2024-12-01 07:34:46 -0300

    listas ajustadas

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -15,9 +15,9 @@ import {ClientesVisualizacaoComponent} from "./components/clientes-visualizacao/
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'entregas', component: EntregasComponent, canActivate: [adminGuard] },
+  { path: 'entregas', component: EntregasComponent, canActivate: [adminGuard]},
   { path: 'entregas/:listaDePedidos', component: EntregasComponent, canActivate: [adminGuard] },
-  { path: 'clientes', component: ClientesListaComponent/*, canActivate: [adminGuard] */},
+  { path: 'clientes', component: ClientesListaComponent, canActivate: [adminGuard] },
   { path: 'clientes-cadastro', component: ClientesCadastroComponent, canActivate: [adminGuard] },
   { path: 'clientes-visualizacao', component: ClientesVisualizacaoComponent, canActivate: [adminGuard] },
   { path: 'clientes-visualizacao/:id', component: ClientesVisualizacaoComponent, canActivate: [adminGuard] },
@@ -27,5 +27,6 @@ export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'map', component: MapComponent },
   { path: 'geo', component: GeoComponent },
-  { path: 'test', component: TestingComponent }
+  { path: 'test', component: TestingComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
